3b5f0d121f37efd046d78c9cf8aa0900f5675384
huyfr <61008601+huyfr@users.noreply.github.com>
2020-04-14 22:19:20 +0700

    Bài tập crawl tin tức
    Sử dụng regex

---
+++ src/Main.java
@@ -0,0 +1,16 @@
+import java.net.MalformedURLException;
+import java.net.URL;
+
+public class Main {
+    public static void main(String[] args) {
+        URL url;
+        CrawlNews crawlNews;
+        try {
+            crawlNews = new CrawlNews();
+            url = new URL("https://dantri.com.vn/the-thao.htm");
+            crawlNews.crawlNews(url);
+        } catch (MalformedURLException e){
+            System.out.println(e.getMessage());
+        }
+    }
+}
